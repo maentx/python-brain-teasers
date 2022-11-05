7ed5876bc5143d2cef6abaddbdcf1b3e54483849
Marian Haentsch <marian.haentsch@gmail.com>
2022-11-06 03:02:24 +0400

    Puzzle 5: A Python program exits when no alive non-daemon threads are left. After the print() line this is exactly the case. Only the first number gets printed and even that is not sure due to the Thread-specific behaviour. Use join() if you want a thread to terminate first.

---
+++ puzzle_5.py
@@ -0,0 +1,14 @@
+#/usr/bin/python
+
+from threading import Thread
+from time import sleep
+
+def printer():
+    for i in range(3):
+        print(i, end=' ')
+        sleep(0.1)
+
+thr = Thread(target=printer, daemon=True)
+thr.start()
+# thr.join()
+print()
