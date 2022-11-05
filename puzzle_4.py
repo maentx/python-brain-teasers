ee05448eda59fa4740edcd27a1d354e5849dcb85
Marian Haentsch <marian.haentsch@gmail.com>
2022-11-05 23:50:09 +0400

    Puzzle 4: Heapq uses the comparison defined in the object's type. The objects in the tasks heap are tuples and Python orders tuples and lists in a lexicographical order. After the first the next two tuples are compared, throwing an error when a string is compared to an integer.

---
+++ puzzle_4.py
@@ -0,0 +1,14 @@
+#/usr/bin/python
+
+from heapq import heappush, heappop
+
+tasks = []
+heappush(tasks, (30, 'work out'))
+heappush(tasks, (10, 'wake up'))
+heappush(tasks, (20, 0xCAFFE))
+heappush(tasks, (20, 'feed cat'))
+heappush(tasks, (40, 'write book'))
+
+while tasks:
+    _, payload = heappop(tasks)
+    print(payload)
